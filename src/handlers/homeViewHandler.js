b6fd1e0a679e5ea32f6b971522f7c11ab70f6df5
Koen Ongena <koen.ongena@imec.be>
2022-07-14 15:48:59 +0200

    Fool-proofing the website

--- src/handlers/homeViewHandler.js
+++ src/handlers/homeViewHandler.js
@@ -1,10 +1,16 @@
 const { parsePdfFile } = require('./pdfHandler');
 
-const post = async (req, res) => {
-  const { pdf } = req.files;
-  const data = await parsePdfFile(pdf);
-
-  res.status(200).render('index', { values: data, filename: pdf.name });
+const post = (req, res) => {
+  if (!req.files) {
+    res.status(400);
+  } else {
+    const { pdf } = req.files;
+    parsePdfFile(pdf).then((data) => {
+      res.status(200).render('index', { values: data, filename: pdf.name });
+    }).catch(() => {
+      res.status(500).render('index', { error: 'Something went wrong. Are you sure this is a valid PDF?' });
+    });
+  }
 };
 
 const get = async (req, res) => {
